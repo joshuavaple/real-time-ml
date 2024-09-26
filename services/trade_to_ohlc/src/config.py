50077f2f9c6830b1e8a221f0f3e249d9101dda38
Joshua Le Ubuntu <phuonglevu3112@gmail.com>
2024-09-26 23:45:10 +0800

    Dockerized the live and historical trade_to_ohlc service

---
+++ services/trade_to_ohlc/src/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+from typing import Optional
+
+
+class AppConfig(BaseSettings):
+    kafka_broker_address: str
+    kafka_input_topic: str
+    kafka_output_topic: str
+    kafka_consumer_group: str
+    ohlcv_window_seconds: int
+    class Config:
+        env_file = '.env'
+        
+config = AppConfig()
